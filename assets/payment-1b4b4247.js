c7f171daf04aac9fac5d5cc6ebde1ecf5fd6bcff
kulasisin <107702039@mail2.nccu.tw>
2023-08-24 19:44:10 +0800

    Updates

---
+++ assets/payment-1b4b4247.js
@@ -0,0 +1,1 @@
+import"./orderSuccess-7a129666.js";$(".needs-validation").each(function(t){$(this).on("submit",function(i){i.preventDefault(),$(this).addClass("was-validated")})});
--- assets/payment-3d82d625.js
+++
@@ -1,1 +0,0 @@
-import"./orderSuccess-f46a5a44.js";$(".needs-validation").each(function(t){$(this).on("submit",function(i){i.preventDefault(),$(this).addClass("was-validated")})});
